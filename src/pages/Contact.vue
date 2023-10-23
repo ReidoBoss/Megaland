619790b22fceda063e61b3a18e229bdecaf943cd
Michael Tonilon <mtonilon17@gmail.com>
2023-10-23 22:22:24 +0800

    to be revised Contacts

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,157 +1,136 @@
-<script lang="ts" setup >
-   
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="centered-container">
-    <div class="page-container">
-      <div class="image-container">
-        <img src="../assets/MegaTouch.png" alt="mgpng" class="mgpng" />
+  <div class="mx-auto max-w-7xl">
+    <div class="relative w-full h-80 bg-cover bg-center bg-custom">
+      <div class="w-[100%] h-3/4">
+        <img
+          src="../assets/mgpng.png"
+          alt="mgpng"
+          class="relative top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-300 z-0 filter-none"
+        />
       </div>
-      
-      <div class="contact-text">
-        <p>📞+63-977-382-4416 📧megalandrealtycebu@gmail.com  📍 Unit 101, 148 Residences, Pelaez St, Cebu City</p>
+
+      <div
+        class="font-bold text-xl flex justify-center align-middle mx-20 m-10"
+      >
+        <p>
+          📞+63-977-382-4416 📧megalandrealtycebu@gmail.com 📍 Unit 101, 148
+          Residences, Pelaez St, Cebu City
+        </p>
       </div>
     </div>
 
-    <div class="form-container">
-    <div class="form-column">
-      <h2>Contact Information</h2>
-      <form @submit.prevent="submitForm">
-        <div class="form-group">
-          <label for="name">Name:</label>
-          <input type="text" id="name" v-model="formData.name" required />
+    <div class="grid">
+      <div class="flex-1 p-20 border border-solid border-[#e67f22] bg-gray-100">
+        <div class="grid-cols-1">
+          <h2 class="font-bold text-xl">Contact Information</h2>
+          <form class="mx-auto" @submit.prevent="submitForm">
+            <div class="mb-10 mt-10">
+              <div class="relative">
+                <input
+                  type="text"
+                  id="name"
+                  class="font-bold block px-2.5 pb-2.5 pt-4 w-[25%] text-sm text-gray-900 bg-transparent rounded-lg border-2 border-[#e67f22] appearance-none dark:text-black dark:border-[#e67f22] dark:focus:border-[#e67f22] focus:outline-none focus:ring-0 focus:border-border-[#e67f22] peer"
+                  v-model="formData.name"
+                  required
+                  placeholder=""
+                />
+                <label
+                  for="name"
+                  class="font-bold absolute text-sm text-[#e67f22] dark:text-[#e67f22] duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg- dark:bg-gray-100 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                  >Name *</label
+                >
+              </div>
+            </div>
+            <div class="mb-10">
+              <div class="relative">
+                <input
+                  type="text"
+                  id="email"
+                  class="font-bold block px-2.5 pb-2.5 pt-4 w-[25%] text-sm text-gray-900 bg-transparent rounded-lg border-2 border-[#e67f22] appearance-none dark:text-black dark:border-[#e67f22] dark:focus:border-[#e67f22] focus:outline-none focus:ring-0 focus:border-border-[#e67f22] peer"
+                  v-model="formData.email"
+                  required
+                  placeholder=""
+                />
+                <label
+                  for="email"
+                  class="font-bold absolute text-sm text-[#e67f22] dark:text-[#e67f22] duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg- dark:bg-gray-100 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                  >Email *</label
+                >
+              </div>
+            </div>
+            <div class="mb-10">
+              <div class="relative">
+                <input
+                  type="text"
+                  id="phone"
+                  class="font-bold block px-2.5 pb-2.5 pt-4 w-[25%] text-sm text-gray-900 bg-transparent rounded-lg border-2 border-[#e67f22] appearance-none dark:text-black dark:border-[#e67f22] dark:focus:border-[#e67f22] focus:outline-none focus:ring-0 focus:border-border-[#e67f22] peer"
+                  v-model="formData.phone"
+                  required
+                  placeholder=""
+                />
+                <label
+                  for="phone"
+                  class="font-bold absolute text-sm text-[#e67f22] dark:text-[#e67f22] duration-300 transform -translate-y-4 scale-75 top-2 origin-[0] bg- dark:bg-gray-100 px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                  >Phone Number *</label
+                >
+              </div>
+            </div>
+          </form>
         </div>
-        <div class="form-group">
-          <label for="email">Email:</label>
-          <input type="email" id="email" v-model="formData.email" required />
-        </div>
-        <div class="form-group">
-          <label for="phone">Phone Number:</label>
-          <input type="tel" id="phone" v-model="formData.phone" required />
-        </div>
-      </form>
-    </div>
+        <div class="right">
+          <h2 class="font-bold text-xl mb-10">Message/Concern</h2>
 
-    <div class="form-column">
-      <h2>Message/Concern</h2>
-      <textarea v-model="formData.message" rows="8"></textarea>
-      <button style="background-color: orange; color: white;">Send</button>
-    </div>      
+          <textarea
+            class="mb-10 font-bold w-[40%] p-10 border-2 border-solid border-[#e67f22] rounded-lg h-40 resize-none"
+            v-model="formData.message"
+            rows="8"
+            placeholder="Write your thoughts..."
+          >
+          </textarea>
+          <div>
+            <button
+              class="text-[#323946] bg-[#e67f22] px-6 py-2 rounded-lg p-4.5 shadow-xl text-2.4 text-2d0d06 leading-7 font-semibold tracking-wide transition-transform transform hover:scale-105 duration-500 ease-in-out box-shadow-custom"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  </template>
- <script>
- export default {
-   data() {
-     return {
-       formData: {
-         name: "",
-         email: "",
-         phone: "",
-         message: "",
-       },
-     };
-   },
-   methods: {
-     submitForm() {
-       // Handle form submission logic here
-       console.log("Form submitted with data:", this.formData);
-       // You can send this data to an API or perform other actions as needed.
-     },
-   },
- };
- </script>
-  
-  <style scoped>
-.centered-container {
-  margin: 0 auto; /* This centers the container horizontally */
-  max-width: 75%; /* You can adjust the maximum width as needed */
-  padding: 20px; /* Optional padding for content inside the container */
-}
-.form-container {
-  display: flex;
-  justify-content: space-between;
-}
+</template>
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+      },
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission logic here
+      console.log("Form submitted with data:", this.formData);
+      // You can send this data to an API or perform other actions as needed.
+    },
+  },
+};
+</script>
 
-.form-column {
-  flex: 1;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: #f9f9f9;
+<style scoped>
+.bg-custom {
+  background-image: url(../assets/conpic.jpg);
 }
-
-.form-group {
-  margin-bottom: 15px;
+.box-shadow-custom {
+  box-shadow: 0 0 10px #e67f22;
 }
-
-label {
-  display: block;
-  font-weight: bold;
+.box-shadow-custom:hover {
+  box-shadow: none;
 }
-
-input, textarea {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-button {
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  border-radius: 5px;
-  cursor: pointer;
-}
-  .contact-text{
-    font-weight: bold;
-    font-size: 20px;
-    display: flex;
-    justify-content: center; /* Horizontal centering */
-    align-items: center;     /* Vertical centering */
-    position: relative;
-  }
-  .page-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 350px;
-    background-image: url('../assets/conpic.jpg'); /* Replace 'your-image.jpg' with the path to your background image */
-    background-size: cover;
-    background-position: relative;
-  }
-  
-  
-  .image-container {
-    width: 100%;
-    height: 25vh; 
-  }
-  
-
-  
-  .mgpng {
-    position: relative;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    max-width: 300%; /* Adjust the size of the logo as needed (increased to 70%) */
-    z-index: 2; /* Ensure the logo appears above the overlay */
-    filter: none; /* Remove grayscale filter for the mgpng element */
-  }
-  
-  .content {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    color: #333;
-    z-index: 3; /* Place the content above the overlay and mgpng */
-  }
-  </style>
-  
-  
+</style>
